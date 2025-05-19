6b8695108b23c097704a592b8cc61917b8661383
Amany-alsayed <159948094+Amany-alsayed@users.noreply.github.com>
2025-05-19 17:28:04 +0300

    "commit test_api file"

---
+++ api/test_api.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+from api import app
+
+client=TestClient(app)
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to the ML Model API"}
+
+
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    json_resp = response.json()
+    assert "status" in json_resp
+    assert json_resp["status"] in ["healthy", "model_not_loaded"]
+
+
