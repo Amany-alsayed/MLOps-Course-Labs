208682fad84ee1e8a51303212257453cbd62b581
Amany-alsayed <159948094+Amany-alsayed@users.noreply.github.com>
2025-05-19 17:10:00 +0300

    "commit api file"

--- api/api.py
+++ api/api.py
@@ -3,7 +3,8 @@ from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
 import joblib
 import uvicorn
-
+from typing import Union
+import pandas as pd
 
 logging.basicConfig(
     level=logging.INFO,
@@ -21,9 +22,16 @@ except Exception as e:
     logger.error(f"failed to load model {e}")
     model=None
 
+try:
+    preprocess=joblib.load("column_transformer.pkl")
+    logger.info("preprocessing file loaded successfully.")
+except Exception as e:
+    logger.error(f"failed to load preprosessing file {e}")
+    preprocess=None
+
 app=FastAPI()
 class PredictRequest(BaseModel):
-    features:list[float]
+    features:list[Union[str, float, int]]
 
 
 @app.get("/")
@@ -34,21 +42,36 @@ async def home():
 
 @app.get("/health")
 async def health():
-    status="healthy" if model else "model not loaded"
+    if model:
+        if preprocess:
+            status="healthy"
+        else:
+            status="preprocessing file not loaded"      
+    else:
+        status="model not loaded"    
     logger.info(f"Health check accessed. Status: {status}")
     return {"status": status}
 
 
-
+feature_columns = [
+    "CreditScore","Geography","Gender","Age","Tenure","Balance",
+    "NumOfProducts","HasCrCard","IsActiveMember","EstimatedSalary"
+    ]
 @app.post("/predict")
 async def predict(request:PredictRequest):
     if not model:
         logger.error("Prediction request received but model not loaded.")
         raise HTTPException(status_code=503, detail="Model not loaded")
+    if not preprocess:
+        logger.error("Prediction request received but preprocessing file not loaded.")
+        raise HTTPException(status_code=503, detail="preprocessing file not loaded")
 
     try:
       logger.info(f"Prediction request received: features={request.features}")
-      prediction=model.predict([request.features])
+      input_df = pd.DataFrame([request.features], columns=feature_columns)
+      processed = preprocess.transform(input_df) 
+      logger.info(f"data after preprocessing phase: features={processed}")
+      prediction=model.predict(processed)
       logger.info(f"Prediction Result: {prediction}")
       return {"prediction": prediction.tolist()}
     except Exception as e:
